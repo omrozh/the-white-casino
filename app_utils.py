27e0ef0cb5006bd6582eaae4da1c66bed11f1139
omrozh <53612475+omrozh@users.noreply.github.com>
2022-07-25 16:05:44 +0300

    Update app_utils.py

--- app_utils.py
+++ app_utils.py
@@ -1,16 +1,43 @@
-from app import Statistics, db, Affiliate, User, UserWallet
+from app import Statistics, db, Affiliate, User, UserWallet, MaxMoneyBet, reflect_bet_loss
 from sys import argv
 from uuid import uuid4
 from app import bcrypt
 from datetime import datetime
+from payments import create_user_wallet, create_btc_wallet
 
+if argv[1] == "init-app":
+    db.drop_all()
+    db.create_all()
+    first_user = User(username="house", email="house@twc.com", password=bcrypt.generate_password_hash("house@twc.2004"),
+                      account_balance=1000, referrer="house", id="house", received_first_time_bonus=True, is_admin=True)
+    new_wallet = create_user_wallet()
+    new_btc_wallet = create_btc_wallet()
+    first_wallet = UserWallet(id="platform-usdt", private_key=new_wallet["private_key"],
+                              public_key=new_wallet["public_key"], address=new_wallet["base58check_address"])
+    btc_first_wallet = UserWallet(id="platform-btc", private_key=new_btc_wallet["private_key"],
+                                  public_key=new_btc_wallet["public_key"],
+                                  address=new_btc_wallet["address"])
+    db.session.add(first_wallet)
+    first_user.usdt_wallet = first_wallet.id
+    first_user.btc_wallet = btc_first_wallet.id
+    db.session.add(first_user)
+    db.session.add(btc_first_wallet)
+    db.session.add(first_wallet)
+    first_affiliate = Affiliate(id="house", amount_wagered=0, connected_account=first_user.id)
+    new_stat = Statistics(id=str(uuid4()), total_amount_wagered=0, number_of_bets=0, year=datetime.now().year,
+                          month=datetime.now().month,
+                          bet_loss=0)
+    db.session.add(new_stat)
+    db.session.add(first_affiliate)
 
 if argv[1] == "add-affiliate":
     first_user = User(username="house", email="house@twc.com", password=bcrypt.generate_password_hash("house@twc.2004"),
-                      account_balance=0, referrer="house", id="house", received_first_time_balance=True)
-    first_wallet = UserWallet(private_key="2db9ae266a2cd722ae5bb28202c3e295850690ee68a8d22832d4ac533993f1ba",
-                              address="0x4c5EfcCEAa910043012515f3E43cFF3F1407bE6B", id=str(uuid4()))
-    first_wallet.usdt_wallet = first_wallet.id
+                      account_balance=0, referrer="house", id="house", received_first_time_bonus=True)
+    new_wallet = create_user_wallet()
+    first_wallet = UserWallet(id=str(uuid4()), private_key=new_wallet["private_key"],
+                              public_key=new_wallet["public_key"], address=new_wallet["base58check_address"])
+    db.session.add(first_wallet)
+    first_user.usdt_wallet = first_wallet.id
     db.session.add(first_user)
     db.session.add(first_wallet)
     first_affiliate = Affiliate(id="house", amount_wagered=0, connected_account=first_user.id)
@@ -29,6 +56,26 @@ if argv[1] == "view-statistics":
               f"Amount Wagered: {'{:.2f}'.format(i.total_amount_wagered)}, Bet Loss: {'{:.2f}'.format(i.bet_loss)} "
               f"| Margin: {house_edge}%")
 
+if argv[1] == "distribute-max-money":
+    all_bets = MaxMoneyBet.query.filter_by(year=datetime.now().year). \
+        filter_by(month=datetime.now().month).filter_by(day=datetime.now().day)
+    current_max_bet = all_bets.first()
+    total_bets = 0
+    for i in all_bets.all():
+        total_bets += i.bet_amount
+        if i.bet_amount > current_max_bet.bet_amount:
+            current_max_bet = i
+    for i in User.query.filter_by(won_max_money_yesterday=True):
+        i.won_max_money_yesterday = False
+
+    winner = User.query.get(current_max_bet.associated_user)
+    winner.won_max_money_yesterday = True
+    winner.account_balance += round((total_bets / 100) * 99, 2)
+    reflect_bet_loss(round((total_bets / 100) * 99, 2))
+    db.session.commit()
+
+    print("Completed")
+
 if argv[1] == "view-affiliate":
     for i in Affiliate.query.all():
         print(f"Amount Wagered: {i.amount_wagered}")
@@ -41,7 +88,7 @@ if argv[1] == "distribute-affiliate-earnings":
     if profit < 0:
         exit()
 
-    distributed_affiliate_profit = (profit / 100) * 70
+    distributed_affiliate_profit = (profit / 100) * 60
     master_affiliate_profit = (profit / 100) * 5
 
     for i in Affiliate.query.all():
